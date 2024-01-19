0e55cd1dfcc177ea3121214402cdc4776d607aa5
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-01-19 19:13:28 +0000

    Added compilation script

---
+++ configs/test/compile.sh
@@ -0,0 +1,19 @@
+# Load modules used for compiling
+module purge
+module load gnu_comp/11.1.0 hdf5/1.12.0 openmpi/4.1.1 cmake/3.25.1
+
+# Location of executable
+mkdir build
+
+# Switch to top directory and clean it
+cd ../../
+make clean
+
+# Create cmake file and configuration
+cmake -B$PWD/configs/test/build -S$PWD
+cmake $PWD/configs/test/build -D HBT_USE_OPENMP=ON -D HBT_UNSIGNED_LONG_ID_OUTPUT=OFF \
+-D HBT_UNSIGNED_LONG_ID_OUTPUT=OFF
+
+# Compile
+cd $PWD/configs/test/build
+make -j 4
