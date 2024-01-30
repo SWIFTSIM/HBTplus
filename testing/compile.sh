dadf256b022563ce138416b298e8d34a3cbde1bd
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-01-30 10:11:10 +0000

    Update relative paths in bash scripts  to reflect folder changes

--- testing/compile.sh
+++ testing/compile.sh
@@ -7,7 +7,7 @@ rm -fr build # In case an older version is already present
 mkdir build
 
 # Create cmake file and configuration
-cmake -B$PWD/build -S"$PWD/../.." -D HBT_USE_OPENMP=ON -D HBT_UNSIGNED_LONG_ID_OUTPUT=OFF
+cmake -B$PWD/build -S"$PWD/.." -D HBT_USE_OPENMP=ON -D HBT_UNSIGNED_LONG_ID_OUTPUT=OFF
 
 # Compile
 cd $PWD/build
