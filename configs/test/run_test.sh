2abe401bedb8130d18c388bde3333b8ca993d27d
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-01-30 09:56:22 +0000

    Test now checks which host the test is being run on. Will complain if not COSMA7, since that is where the test data is located.

--- configs/test/run_test.sh
+++ configs/test/run_test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-bash compile.sh
-sbatch submit_test.sh
+
+if [[ $(hostname) != *"login7"* ]]
+then
+    echo "Trying to run test in a non-COSMA7 login node. Please switch to COSMA7."
+else
+    bash compile.sh
+    sbatch submit_test.sh
+fi
