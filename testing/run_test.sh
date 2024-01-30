50c0d5da9374490bef1bca04195a499b6f0eb30f
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-01-30 15:41:07 +0000

    Remove outputs from any pre-existing tests.

--- testing/run_test.sh
+++ testing/run_test.sh
@@ -4,6 +4,7 @@ if [[ $(hostname) != *"login7"* ]]
 then
     echo "Trying to run test in a non-COSMA7 login node. Please switch to COSMA7."
 else
+    rm -rf test_output # Remove to prevent partially overwritting previous tests.
     bash compile.sh
     sbatch submit_test.sh
 fi
