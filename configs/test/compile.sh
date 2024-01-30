fa5543646d8b3cb2e53a2929d47333d78d7e5ae7
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-01-30 09:43:44 +0000

    Compilation only writes .o files in build directory

--- configs/test/compile.sh
+++ configs/test/compile.sh
@@ -6,13 +6,9 @@ module load gnu_comp/11.1.0 hdf5/1.12.0 openmpi/4.1.1 cmake/3.25.1
 rm -fr build # In case an older version is already present
 mkdir build
 
-# Switch to top directory and clean it
-cd ../../
-make clean
-
 # Create cmake file and configuration
-cmake -B$PWD/configs/test/build -S$PWD -D HBT_USE_OPENMP=ON -D HBT_UNSIGNED_LONG_ID_OUTPUT=OFF
+cmake -B$PWD/build -S"$PWD/../.." -D HBT_USE_OPENMP=ON -D HBT_UNSIGNED_LONG_ID_OUTPUT=OFF
 
 # Compile
-cd $PWD/configs/test/build
+cd $PWD/build
 make -j 4
