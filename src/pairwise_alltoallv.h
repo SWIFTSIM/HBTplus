2ed8f582699c4050fd826525641dad3b499effb3
John Helly <j.c.helly@durham.ac.uk>
2024-03-06 11:37:14 +0000

    Add function for rend/recv displacements, with unit test

--- src/pairwise_alltoallv.h
+++ src/pairwise_alltoallv.h
@@ -75,9 +75,45 @@ int Pairwise_Alltoallv(const void *sendbuf, const T *sendcounts, const T *sdispl
   This version accepts vector arguments. Recvbuf must already be large enough for the result.
 */
 template<typename T, typename U, typename V>
-int Pairwise_Alltoallv(const std::vector<U> sendbuf, const std::vector<T> sendcounts, const std::vector<T> sdispls, MPI_Datatype sendtype,
-                       std::vector<V> recvbuf, const std::vector<T>recvcounts, const std::vector<T> rdispls, MPI_Datatype recvtype, MPI_Comm comm)
+int Pairwise_Alltoallv(const std::vector<U> &sendbuf, const std::vector<T> &sendcounts, const std::vector<T> &sdispls, MPI_Datatype sendtype,
+                       std::vector<V> &recvbuf, const std::vector<T> &recvcounts, const std::vector<T> &rdispls, MPI_Datatype recvtype, MPI_Comm comm)
 {
   return Pairwise_Alltoallv(sendbuf.data(), sendcounts.data(), sdispls.data(), sendtype,
                             recvbuf.data(), recvcounts.data(), rdispls.data(), recvtype, comm);
+}
+
+/*
+  Given number of elements to send to each rank, compute send displacements and receive counts and displacements
+*/
+template<typename T>
+void ExchangeCounts(const std::vector<T> &sendcounts, std::vector<T> &sdispls,
+                    std::vector<T> &recvcounts, std::vector<T> &rdispls,
+                    MPI_Comm comm) {
+
+  int comm_size;
+  MPI_Comm_size(comm, &comm_size);
+  int comm_rank;
+  MPI_Comm_rank(comm, &comm_rank);
+
+  // Exchange counts
+  std::vector<long long> sendcounts_ll(comm_size);
+  std::vector<long long> recvcounts_ll(comm_size);
+  for(int i=0; i<comm_size; i+=1)
+    sendcounts_ll[i] = static_cast<long long>(sendcounts[i]);
+  MPI_Alltoall(sendcounts_ll.data(), 1, MPI_LONG_LONG, recvcounts_ll.data(), 1, MPI_LONG_LONG, comm);
+
+  // Ensure output vectors are the right size
+  sdispls.resize(comm_size);
+  recvcounts.resize(comm_size);
+  rdispls.resize(comm_size);
+  
+  // Populate output vectors
+  for(int i=0; i<comm_size; i+=1)
+    recvcounts[i] = static_cast<T>(recvcounts_ll[i]);
+  sdispls[0] = 0;
+  rdispls[0] = 0;
+  for(int i=0; i<comm_size; i+=1) {
+    sdispls[i] = sdispls[i-1] + sendcounts[i-1];
+    rdispls[i] = rdispls[i-1] + recvcounts[i-1];    
+  }
 }
