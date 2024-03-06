4ffd1d721aac9a30d09f62d92b6907ef6c0ed4b0
John Helly <j.c.helly@durham.ac.uk>
2024-03-06 15:17:47 +0000

    Start implementing function for looking up values by ID

--- src/locate_ids.h
+++ src/locate_ids.h
@@ -43,10 +43,84 @@ void LocateValuesById(const std::vector<HBTInt> &ids,
                       std::vector<HBTInt> &ids_found,
                       std::vector<T> &values_found,
                       MPI_Comm comm) {
+
+  int comm_size;
+  MPI_Comm_size(comm, &comm_size);
+  int comm_rank;
+  MPI_Comm_rank(comm, &comm_rank);
+
+  //
+  // First we redistribute the IDs and values according to the hash of the ID.
+  //
+  std::vector<T> values_recvbuf(0);
+  std::vector<HBTInt> ids_recvbuf(0);
+  {
+    // Count the number of elements from ids and values to send to each rank.
+    std::vector<HBTInt> sendcounts(comm_size, 0);
+    for(auto id: ids) {
+      HBTInt hash = HashInteger(id);
+      int dest = (hash % comm_size);
+      sendcounts[dest] += 1;
+    }
+
+    // Compute displacements for the exchange
+    std::vector<HBTInt> senddispls(comm_size);  
+    std::vector<HBTInt> recvcounts(comm_size);
+    std::vector<HBTInt> recvdispls(comm_size);  
+    ExchangeCounts(sendcounts, senddispls, recvcounts, recvdispls, comm);
+
+    // Compute totals to send and receive
+    HBTInt total_nr_send = 0;
+    for(auto count: sendcounts)
+      total_nr_send += count;
+    HBTInt total_nr_recv = 0;
+    for(auto count: recvcounts)
+      total_nr_recv += count;
+
+    // Allocate buffer to receive IDs
+    ids_recvbuf.resize(total_nr_recv);
+    {
+      // Populate send buffer for the IDs
+      std::vector<HBTInt> ids_sendbuf(total_nr_send);
+      std::vector<HBTInt> offset = senddispls;
+      for(HBTInt i=0; i<ids.size(); i+=1) {
+        HBTInt hash = HashInteger(ids[i]);
+        int dest = (hash % comm_size);
+        ids_sendbuf[offset[dest]] = ids[i];
+        offset[dest] += 1;
+      }
+      // Exchange IDs
+      Pairwise_Alltoallv(ids_sendbuf, sendcounts, senddispls, MPI_HBT_INT,
+                         ids_recvbuf, recvcounts, recvdispls, MPI_HBT_INT,
+                         comm);
+    }
+
+    // Allocate buffer to receive values
+    values_recvbuf.resize(total_nr_recv);
+    {
+      // Populate send buffer for the values
+      std::vector<T> values_sendbuf(total_nr_send);
+      std::vector<HBTInt> offset = senddispls;
+      for(HBTInt i=0; i<ids.size(); i+=1) {
+        HBTInt hash = HashInteger(ids[i]);
+        int dest = (hash % comm_size);
+        values_sendbuf[offset[dest]] = values[i];
+        offset[dest] += 1;
+      }
+      // Exchange values
+      Pairwise_Alltoallv(values_sendbuf, sendcounts, senddispls, mpi_value_type,
+                         values_recvbuf, recvcounts, recvdispls, mpi_value_type,
+                         comm);
+    }
+  }
   
+  //
+  // Now we need to redistribute the IDs to find in the same way
+  //
   
-  
-  
+
+
+
 }
 
 #endif
