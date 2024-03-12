4ce411c26149a49ca05445383bcf3c61608146d9
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-03-12 10:04:50 +0000

    Number of tracers used for host finding can be specified in config. If unspecified, it will default to the minimum number of tracers required in each subhalo.

--- src/config_parser.h
+++ src/config_parser.h
@@ -42,6 +42,7 @@ public:
   int MinSnapshotIndex;
   int MinNumPartOfSub;
   int MinNumTracerPartOfSub;
+  int NumTracerHostFinding;
   long GroupParticleIdMask; // only used for a peculiar gadget format.
   HBTReal MassInMsunh;
   HBTReal LengthInMpch;
@@ -92,6 +93,7 @@ public:
     MinSnapshotIndex = 0;
     MinNumPartOfSub = 20;
     MinNumTracerPartOfSub = 10;
+    NumTracerHostFinding = MinNumTracerPartOfSub;
     GroupParticleIdMask = 0;
     MassInMsunh = 1e10;
     LengthInMpch = 1;
