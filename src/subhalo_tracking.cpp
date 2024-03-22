49c5f5e62f7aa646c0a3b9f159d2573c5fd731dc
robjmcgibbon <robjmcgibbon@gmail.com>
2024-03-22 08:18:48 +0000

    Iterate over all particles to find most bound tracer

--- src/subhalo_tracking.cpp
+++ src/subhalo_tracking.cpp
@@ -526,13 +526,12 @@ void SubhaloSnapshot_t::FeedCentrals(HaloSnapshot_t &halo_snap)
     {
       auto &central = Subhalos[Members[0]];
       assert(central.Particles.size());
-      HBTInt tracerIndex = central.GetTracerIndex(); // Save tracerIndex before swap
       central.Particles.swap(Host.Particles);        // reuse the halo particles
       central.Nbound = central.Particles.size();
       bool tracerIndexSet = false;
       // Use the most bound tracer from the previous snapshot that remains
       // in the FOF group as the tracer. We need this information for the masking.
-      for (HBTInt i = tracerIndex; i < Host.Particles.size(); i++)
+      for (HBTInt i = 0; i < Host.Particles.size(); i++)
       {
         if (tracerIndexSet)
           break;
