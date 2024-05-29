732476f00dcfb3a4eea426b5d40ccf55eb3f7658
John Helly <j.c.helly@durham.ac.uk>
2024-05-29 16:24:52 +0100

    Modify MaxConcurrentIO implementation for swift
    The previous implementation split the MPI ranks into groups and
    prevented any ranks in the next group from proceeding until all
    ranks in the current group have finished. This will waste a lot
    of time if one (or a few) ranks are very slow.
    
    This new implementation tries to make sure that we always have
    MaxConcurrentIO ranks reading. When any one rank finishes the
    next is allowed to start immediately.

---
+++ src/task_limited_section.h
@@ -0,0 +1,131 @@
+#include <mpi.h>
+
+class TaskLimitedSection {
+
+private:
+
+  int max_nr_tasks;
+  MPI_Comm comm;
+  MPI_Win win;
+  int *buffer;
+  int controller_rank;
+  MPI_Request controller_rank_request;
+
+  const int CONTROLLER_RANK_TAG = 0;
+  const int GO_TAG = 1;
+  const int COMPLETION_TAG = 2;
+
+public:
+
+  TaskLimitedSection(MPI_Comm comm, const int max_nr_tasks) {
+
+    MPI_Comm_dup(comm, &(this->comm));
+    this->max_nr_tasks = max_nr_tasks;
+  }
+  
+  void start() {
+  
+    /* Get rank and number of ranks */
+    int comm_size;
+    MPI_Comm_size(comm, &comm_size);
+    int comm_rank;
+    MPI_Comm_rank(comm, &comm_rank);
+
+    /* If all ranks are allowed to run there's nothing to do */
+    if(max_nr_tasks >= comm_size)return;
+
+    /* Allocate and init counter for RMA */
+    MPI_Alloc_mem(sizeof(int), MPI_INFO_NULL, &buffer);
+    *buffer = 0;
+    MPI_Win_create(buffer, sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+
+    /* Post a receive to get controller task's rank (will be first rank to finish) */
+    MPI_Irecv(&controller_rank, 1, MPI_INT, MPI_ANY_SOURCE,
+              CONTROLLER_RANK_TAG, comm, &controller_rank_request);
+    
+    /* The first max_nr_tasks ranks can proceed immediately */
+    if(comm_rank < max_nr_tasks)return;
+
+    /* Others need to wait for a message to proceed */
+    int go;
+    MPI_Recv(&go, 1, MPI_INT, MPI_ANY_SOURCE, GO_TAG, comm, MPI_STATUS_IGNORE);
+    
+  }
+  
+  void end() {
+
+    /* Get rank and number of ranks */
+    int comm_size;
+    MPI_Comm_size(comm, &comm_size);
+    int comm_rank;
+    MPI_Comm_rank(comm, &comm_rank);
+
+    /* If all ranks are allowed to run there's nothing to do */
+    if(max_nr_tasks >= comm_size)return;
+
+    MPI_Request *request = (MPI_Request *) malloc(sizeof(MPI_Request)*comm_size);
+  
+    /*
+      Check if we're the first task to reach the end of the section:
+      We do this by doing an atomic fetch and increment on the count of
+      the number of ranks that have finished. If the count is zero we're
+      the first and will become responsible for signalling other ranks
+      to proceed.
+      
+      Hopefully this shouldn't block because the rank with the counter
+      is sitting waiting at a blocking receive.
+    */
+    int completion_count = 0;
+    MPI_Win_lock(MPI_LOCK_EXCLUSIVE, comm_size-1, 0, win);
+    int to_add = 1;
+    MPI_Get_accumulate(&to_add, 1, MPI_INT,
+                       &completion_count, 1, MPI_INT,
+                       comm_size-1, 0, 1, MPI_INT, MPI_SUM, win);
+    MPI_Win_unlock(comm_size-1, win);
+    if(completion_count == 0) {
+      
+      /* This task is the first to reach the end of the section, so tell everyone */
+      for(int dest=0; dest<comm_size; dest+=1)
+        MPI_Isend(&comm_rank, 1, MPI_INT, dest, CONTROLLER_RANK_TAG, comm, request+dest);
+
+      /* Then we need to wait for others to finish and send go signals as necessary */
+      int nr_left = comm_size - max_nr_tasks;
+      for(int i=0; i<comm_size; i+=1) {
+        
+        /* Wait for a completion message (but not on first iteration, because we don't send to self) */
+        if(i > 0) {
+          int done;
+          MPI_Status status;
+          MPI_Recv(&done, 1, MPI_INT, MPI_ANY_SOURCE, COMPLETION_TAG, comm, &status);
+        }
+        
+        /* If there are tasks still waiting, send the next go signal */
+        if(nr_left > 0) {
+          int dest = comm_size - nr_left;
+          int go = 1;
+          MPI_Send(&go, 1, MPI_INT, dest, GO_TAG, comm);
+          nr_left -= 1;
+        }
+      }
+    }
+
+    /* Make sure we've received the controller rank */
+    MPI_Wait(&(controller_rank_request), MPI_STATUS_IGNORE);
+  
+    /* Send completion message if we're not the controller */
+    if(completion_count > 0) {
+      int complete = 1;
+      MPI_Send(&complete, 1, MPI_INT, controller_rank, COMPLETION_TAG, comm);
+    }
+
+    /* Make sure all sends from the controller completed */
+    if(completion_count==0)
+      MPI_Waitall(comm_size, request, MPI_STATUSES_IGNORE);
+  
+    /* Tidy up */
+    free(request);
+    MPI_Win_free(&(win));
+    MPI_Free_mem(buffer);
+  }
+  
+};
