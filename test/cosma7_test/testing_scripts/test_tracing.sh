a38021f499cdcb9543154e6e9b740a1ea26c9898
Victor Forouhar <forouhar@strw.leidenuniv.nl>
2024-12-11 13:27:49 +0000

    Update snapshot number

--- test/cosma7_test/testing_scripts/test_tracing.sh
+++ test/cosma7_test/testing_scripts/test_tracing.sh
@@ -9,20 +9,20 @@ indir="${base_folder}/HBT" # HBT catalogue
 snap_files="${base_folder}/colibre_{snap_nr:04d}.hdf5" # SWIFT snapshots
 
 # Tests whether particles of subgroups are confined to their host group (or are not part of any FOF)
-mpirun -np 32 python3 -m mpi4py ./check_interhost_subhalos.py "${indir}" 126 126 --snapshot-file="${snap_files}";
+mpirun -np 32 python3 -m mpi4py ./check_interhost_subhalos.py "${indir}" 127 127 --snapshot-file="${snap_files}";
 echo
 
 # Tests whether there are any duplicated particles in the HBT catalogues.
-mpirun -np 32 python3 -m mpi4py ./check_presence_duplicate_particles.py "${indir}" 126;
+mpirun -np 32 python3 -m mpi4py ./check_presence_duplicate_particles.py "${indir}" 127;
 echo
 
 # Tests whether the ID of the tracer used for orphaned subgroups remains unchanged.
-mpirun -np 32 python3 -m mpi4py ./check_consistency_particles_orphan_subgroups.py "${indir}" 126;
+mpirun -np 32 python3 -m mpi4py ./check_consistency_particles_orphan_subgroups.py "${indir}" 127;
 echo
 
 # Tests correctness of assigned host groups for orphaned subgroups.
-mpirun -np 32 python3 -m mpi4py ./check_tracing_particles_orphan_subgroups.py "${indir}" 126 126 --snapshot-file="${snap_files}";
+mpirun -np 32 python3 -m mpi4py ./check_tracing_particles_orphan_subgroups.py "${indir}" 127 127 --snapshot-file="${snap_files}";
 echo
 
 # Tests correctness of assigned host groups for resolved subgroups.
-mpirun -np 32 python3 -m mpi4py ./check_tracing_particles_resolved_subgroups.py "${indir}" 126 126 --snapshot-file="${snap_files}";
+mpirun -np 32 python3 -m mpi4py ./check_tracing_particles_resolved_subgroups.py "${indir}" 127 127 --snapshot-file="${snap_files}";
